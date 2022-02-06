e5fd75bfde4b9d1ba148a0ce309b5ad0d75ac80a
Onur Baskirt <onur.baskirt@dnata.com>
2022-02-06 20:48:44 +0400

    Navigation tests were fixed by adding wait statements.

--- src/test/java/navigation/NavigationTests.java
+++ src/test/java/navigation/NavigationTests.java
@@ -1,6 +1,7 @@
 package navigation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,19 +11,22 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class NavigationTests {
 
-    private       WebDriver driver;
-    final private String    URL1 = "http://www.yahoo.com";
-    final private String    URL2 = "http://www.amazon.com";
+    private       WebDriver     driver;
+    private       WebDriverWait wait;
+    final private String        URL1 = "http://www.yahoo.com";
+    final private String        URL2 = "http://www.amazon.com";
 
     @BeforeAll
     public void setupTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     //.get Example
@@ -42,7 +46,7 @@ public class NavigationTests {
         driver.navigate().to(URL2);
 
         //Check title is correct
-        Assertions.assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
+        Assertions.assertEquals("Amazon.com. Spend less. Smile more.", driver.getTitle());
     }
 
     @Test
@@ -50,27 +54,32 @@ public class NavigationTests {
     public void T03_backForwardRefreshExample() {
         //Go to www.yahoo.com
         driver.navigate().to(URL1);
+        wait.until(driver -> driver.getTitle().contentEquals("Yahoo"));
         //Check title is correct
         Assertions.assertEquals(driver.getTitle(), "Yahoo");
 
         //Go to www.amazon.com
         driver.navigate().to(URL2);
+        wait.until(driver -> driver.getTitle().contentEquals("Amazon.com. Spend less. Smile more."));
         //Check title is correct
-        Assertions.assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
+        Assertions.assertEquals("Amazon.com. Spend less. Smile more.", driver.getTitle());
 
         //***Navigate Back***
         driver.navigate().back();
+        wait.until(driver -> driver.getTitle().contentEquals("Yahoo"));
         //Check title is correct
         Assertions.assertEquals(driver.getTitle(), "Yahoo");
 
         //***Navigate Forward***
         driver.navigate().forward();
+        wait.until(driver -> driver.getTitle().contentEquals("Amazon.com. Spend less. Smile more."));
         //Check title is correct
-        Assertions.assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
+        Assertions.assertEquals("Amazon.com. Spend less. Smile more.", driver.getTitle());
 
         //***Refresh The Page***
         driver.navigate().refresh();
-        Assertions.assertEquals(driver.getTitle(), "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more");
+        wait.until(driver -> driver.getTitle().contentEquals("Amazon.com. Spend less. Smile more."));
+        Assertions.assertEquals("Amazon.com. Spend less. Smile more.", driver.getTitle());
     }
 
     @AfterAll
